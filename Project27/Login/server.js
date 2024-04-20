f3aa8be4a9d21c6f0455c5c3964d78f127b2fd19
Nikhil Verma <155971659+nikhilcse0427@users.noreply.github.com>
2024-04-20 22:50:18 +0530

    Add files via upload

---
+++ Project27/Login/server.js
@@ -0,0 +1,46 @@
+const express= require("express")
+require("./connect")
+const app = express();
+const body1=require('body-parser');
+const Student1 = require("./connect");
+const encoded=body1.urlencoded({extended:false})
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname+'/signup.html');
+})
+app.post('/signup',encoded,async (req,res)=>{
+    let student = await Student1(req.body);
+    student.save()
+        .then(() => {
+            res.send(`
+            <h2>User registered successfully!</h2>
+            <p>Click <a href="/login">here</a> to login or click <a href="/">here</a> for register another user.</p>
+        `);
+        })
+        .catch(err => console.log(err))
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname+'/login.html')
+})
+app.post('/loggedin', encoded, async (req,res)=>{
+    const username1=req.body.username;
+    const password1 = req.body.password;
+    Student1.findOne({ email:username1, password:password1 })
+        .then(student => {
+            if (student) {
+                
+                res.redirect('/dashboard');
+            } else {
+                res.status(401).send('Invalid username or password');
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        });
+})
+app.get('/dashboard', (req,res)=>{
+    res.send("Welcome User")
+})
+app.listen(8095, ()=>{
+    console.log("Server is rumming on port 8095")
+})
